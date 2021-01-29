194bde1e5bae713bacbfac0091c9ae3d633e4135
sgreenlund3 <69866618+sgreenlund3@users.noreply.github.com>
2021-01-29 14:32:07 -0700

    comments and cleanup

--- Design Challenge 2/neopixel_lightsensor/neopixel_lightsensor.ino
+++ Design Challenge 2/neopixel_lightsensor/neopixel_lightsensor.ino
@@ -1,25 +1,20 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <movingAvg.h>
 
-
 //#include <Adafruit_NeoPixel.h> dont use this with circuit playground, causes errors
 
+// set up for the light strip
 #define NEOPIX_PIN    A9
 #define NUM_PIXELS 5
 
-int lightValue;
-
-//int red = 86;
-//int green = 78;
-//int blue = 109;
-
-
 Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(NUM_PIXELS, NEOPIX_PIN, NEO_GRB + NEO_KHZ800);
 
+// use the movingAvg library to get an average light value instead of instantaneous
 movingAvg mySensor(100);
 
 void setup() {
-  Serial.begin(115200);
+  //initiate libraries
+  Serial.begin(115200); //I want this to turn on quickly after opening closet
   CircuitPlayground.begin();
   strip.begin();  
   mySensor.begin();                   
@@ -27,16 +22,16 @@ void setup() {
 }
 
 void loop() {
-  int sensorData = analogRead(A5);
-  int sensorMovingAvg = mySensor.reading(sensorData);
-  Serial.println(sensorMovingAvg);
+  int sensorData = analogRead(A5); //light sensor is in analog A5 on the circuit playground
+  int sensorMovingAvg = mySensor.reading(sensorData); //get the light values
+  Serial.println(sensorMovingAvg); //print the moving average (for reference)
 
+  //turn on lights if value is over 10 and turn them off if lower 
   if (sensorMovingAvg >10) {  
     //apply rgb colors
     for(int i=0; i<5; i++) {
       strip.setPixelColor(i, 0x913d9f); 
-    }
-           
+    }       
     for(int i=0; i<9; i++) {
       CircuitPlayground.setPixelColor(i, 0x913d9f);         
     } 
